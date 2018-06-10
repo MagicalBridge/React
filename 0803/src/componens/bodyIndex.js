89684401a4426198c2db24961f1202f07b810261
褚鹏飞 <540788769@qq.com>
2018-06-10 13:05:24 +0800

    commit

--- 0803/src/componens/bodyIndex.js
+++ 0803/src/componens/bodyIndex.js
@@ -1,14 +1,16 @@
 import React from 'react' // 引入依赖
-import ReactDOM from 'react-dom' // 引入reactdom依赖
+import Child from '../componens/child';
 // 扩展一个组件
 export default class BodyIndex extends React.Component {
   constructor(){
     super();
     this.state = {age:24}
   }
-
-  changeUserInfo(){
-    this.setState({age:32});
+  handleChildvalueChange(event){
+    this.setState({age:event.target.value});
+  }
+  changeUserInfo(age){
+    this.setState({age:age});
   }
   render() { // render 函数
     return ( // 返回的jsx语法
@@ -16,7 +18,8 @@ export default class BodyIndex extends React.Component {
         <h2>这是中间部分的内容信息</h2>
         <p>{this.props.info}</p>
         <p>age:{this.state.age}</p>
-        <input type="button" value="提交" onClick={this.changeUserInfo.bind(this)} />
+        <input type="button" value="提交" onClick={this.changeUserInfo.bind(this,88)} />
+        <Child handleChildvalueChange = {this.handleChildvalueChange.bind(this)} />
       </div>
     )
   }
