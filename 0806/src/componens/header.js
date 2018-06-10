5100f41c39af353158f72adf61cbd056f81b730a
褚鹏飞 <540788769@qq.com>
2018-06-10 14:48:53 +0800

    Commit

---
+++ 0806/src/componens/header.js
@@ -0,0 +1,12 @@
+import React from 'react' // 引入依赖
+import ReactDOM from 'react-dom' // 引入reactdom依赖
+// 扩展一个组件
+export default class ComponentHeader extends React.Component{
+  render(){ // render 函数
+    return ( // 返回的jsx语法
+      <header>
+        <h1>这是头部</h1>
+      </header>
+    )
+  }
+}
