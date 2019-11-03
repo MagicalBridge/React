0392c513010b61955266ca04f31d0e6a8e2470e3
褚鹏飞 <450793585@qq.com>
2019-11-03 18:28:57 +0800

    路由嵌套

--- react-router-demo/src/App.js
+++ react-router-demo/src/App.js
@@ -1,43 +1,35 @@
 import React from 'react';
-// import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom'
 import {
   HashRouter as Router,
-  Link,
   Switch,
   Route,
+  Link,
   useParams,
   useRouteMatch
-} from 'react-router-dom'
+} from "react-router-dom";
 import './App.css';
 
+// 下面这个组件的 实现的功能是 默认显示的是一级路由 home 和 topics 
+// 点击topics 之后 会显示 是哪个tab 起初因为 匹配的和父路由使用的是同样的方式
+// 后来 通过点击Topics 组件中的三个tab 路由传递的参数发生了改变,
 function App() {
   return (
     <Router>
       <div className="App">
-        <h2>我们</h2>
         <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/topics">Topics</Link>
-          </li>
+          <li> <Link to="/">Home</Link> </li>
+          <li><Link to="/topics">Topics</Link> </li>
         </ul>
-        <br />
+        <hr />
         <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/topics">
-            <Topics />
-          </Route>
+          <Route exact path="/" component={Home} />
+          <Route path="/topics" component={Topics} />
         </Switch>
       </div>
     </Router>
   );
 }
 
-// home 组件
 function Home() {
   return (
     <div>
@@ -45,27 +37,15 @@ function Home() {
     </div>
   );
 }
-
-function Topic() {
-  // The <Route> that rendered this component has a
-  // path of `/topics/:topicId`. The `:topicId` portion
-  // of the URL indicates a placeholder that we can
-  // get from `useParams()`.
-  let { topicId } = useParams();
-
-  return (
-    <div>
-      <h3>{topicId}</h3>
-    </div>
-  );
-}
-
-// 在Topics 组件中还可以嵌套 link使用 
 function Topics() {
   // The `path` lets us build <Route> paths that are
   // relative to the parent route, while the `url` lets
   // us build relative links.
   let { path, url } = useRouteMatch();
+
+  // 下面打印的 是相同的结果
+  console.log(path);
+  console.log(url);
 
   return (
     <div>
@@ -86,11 +66,23 @@ function Topics() {
         <Route exact path={path}>
           <h3>Please select a topic.</h3>
         </Route>
-        <Route path={`${path}/:topicId`}>
-          <Topic />
-        </Route>
+        <Route path={`${path}/:topicId`} component={Topic}/>
       </Switch>
     </div>
   );
 }
+
+function Topic() {
+  // The <Route> that rendered this component has a
+  // path of `/topics/:topicId`. The `:topicId` portion
+  // of the URL indicates a placeholder that we can
+  // get from `useParams()`.
+  let { topicId } = useParams();
+  return (
+    <div>
+      <h3>{topicId}</h3>
+    </div>
+  );
+}
+
 export default App;
