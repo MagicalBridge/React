7d3a6b468bca289bc9f94291601bb48cf2fc0702
褚鹏飞 <450793585@qq.com>
2019-11-03 18:00:35 +0800

    Revert "url-parameters"
    This reverts commit 7546f29332b9350514c03c0c2c1a21f3892ef361.

--- react-router/router-demo/src/App.js
+++ react-router/router-demo/src/App.js
@@ -1,52 +1,59 @@
 import React from 'react';
 // import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom'
-import {
-  HashRouter as Router,
-  Link,
-  Switch,
-  Route,
-  useParams
-} from 'react-router-dom'
+import { HashRouter as Router, Link, Switch, Route } from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <h2>Accounts</h2>
         <ul>
           <li>
-            <Link to="/netflix">Netflix</Link>
+            <Link to="/" >Home</Link>
           </li>
           <li>
-            <Link to="/zillow-group">Zillow Group</Link>
+            <Link to="/about" >About</Link>
           </li>
           <li>
-            <Link to="/yahoo">Yahoo</Link>
-          </li>
-          <li>
-            <Link to="/modus-create">Modus Create</Link>
+            <Link to="/dashboard" >Dashboard</Link>
           </li>
         </ul>
         <br />
         <Switch>
-          <Route path="/:id" children={<Child />} />
+          <Route exact path="/" component={Home}/>
+          <Route path="/about">
+            <About></About>
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard></Dashboard>
+          </Route>
         </Switch>
       </div>
     </Router>
   );
 }
 
-function Child() {
-  // We can use the `useParams` hook here to access
-  // the dynamic pieces of the URL.
-  // 我们可以使用 useParams 这个钩子函数来访问url的动态参数
-  let { id } = useParams();
-
+function Home() {
   return (
     <div>
-      <h3>ID: {id}</h3>
+      Home
     </div>
-  );
+  )
+}
+
+function About() {
+  return (
+    <div>
+      About
+    </div>
+  )
+}
+
+function Dashboard() {
+  return (
+    <div>
+      Dashboard
+    </div>
+  )
 }
 export default App;
